fffd71dbbb5b5cf0a6289b858770f7158e04f26d
dev-sccm <marcos.mol@compasso.com.br>
2020-06-22 23:48:33 -0300

    Commit inicial

---
+++ src/main/java/br/com/teste/Teste/Banco/Inter/TesteBancoInterApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste.Teste.Banco.Inter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBancoInterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBancoInterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/teste/Teste/Banco/Inter/TesteBancoInterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.Teste.Banco.Inter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteBancoInterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
